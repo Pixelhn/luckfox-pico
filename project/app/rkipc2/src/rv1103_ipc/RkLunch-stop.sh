e9932572ec575b7ddb40d54339629a8902cdada2
lhn_wsl <pixelhn@qq.com>
2024-06-24 22:57:12 +0800

    调整app编译

--- project/app/rkipc2/src/rv1103_ipc/RkLunch-stop.sh
+++ project/app/rkipc2/src/rv1103_ipc/RkLunch-stop.sh
@@ -24,4 +24,27 @@ rcK()
 	done
 }
 
+echo "Stop Application ..."
+killall rkipc
+
+cnt=0
+while [ 1 ];
+do
+	sleep 1
+	cnt=$(( cnt + 1 ))
+	if [ $cnt -eq 8 ]; then
+		echo "killall -9 rkipc"
+		killall -9 rkipc
+		sleep 0.1
+		break
+	fi
+	ps|grep rkipc|grep -v grep
+	if [ $? -ne 0 ]; then
+		echo "rkipc exit"
+		break
+	else
+		echo "rkipc active"
+	fi
+done
+
 rcK
