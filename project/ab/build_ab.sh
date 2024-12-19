a9fa2db37f2028497b1df6a18ae5e9c0c59c13a8
lhn_wsl <pixelhn@qq.com>
2024-12-19 22:03:29 +0800

    system a b

--- project/ab/build_ab.sh
+++ project/ab/build_ab.sh
@@ -2,4 +2,7 @@
 
 ubinize -o system.ubi -m 2048 -p 0x20000 -v system.cfg || exit
 
-mv system.ubi ../../output/image/oem.img
+cp system.ubi ./out/system_a.img
+cp system.ubi ./out/system_b.img
+
+../../output/out/sysdrv_out/pc/mkenvimage -s 0x40000 -p 0x0 -o out/env.img env.txt
