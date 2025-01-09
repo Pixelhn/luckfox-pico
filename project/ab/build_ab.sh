296109cfa46340f3bb0d11565375c4459810f2af
lhn_debian12 <pixelhn@qq.com>
2025-01-09 10:12:33 +0800

    ab

--- project/ab/build_ab.sh
+++ project/ab/build_ab.sh
@@ -2,7 +2,6 @@
 
 ubinize -o system.ubi -m 2048 -p 0x20000 -v system.cfg || exit
 
-cp system.ubi ./out/system_a.img
-cp system.ubi ./out/system_b.img
+cp system.ubi ./out/system.img
 
 ../../output/out/sysdrv_out/pc/mkenvimage -s 0x40000 -p 0x0 -o out/env.img env.txt
