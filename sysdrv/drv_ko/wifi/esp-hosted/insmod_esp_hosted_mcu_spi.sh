e4a508ac843aef8686bbe78d384c9a3e3d7a9ab0
lhn <pixelhn@qq.com>
2026-09-12 16:20:23 +0800

    esp-hosted编译测试程序

---
+++ sysdrv/drv_ko/wifi/esp-hosted/insmod_esp_hosted_mcu_spi.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+RSETET_PIN=54
+SPI_HANDSHAKE_PIN=34
+SPI_DATAREADY_PIN=4
+SPI_CS_PIN=0
+SPI_CLOCKS_SPEED=20
+SPI_BUS=0
+SPI_MODE=0
+
+
+cd /oem/usr/ko/
+insmod esp32_spi.ko resetpin=${RSETET_PIN} \
+    spi_bus=${SPI_BUS} \
+    spi_cs=${SPI_CS_PIN} \
+    spi_mode=${SPI_MODE} \
+    spi_handshake=${SPI_HANDSHAKE_PIN} \
+    spi_dataready=${SPI_DATAREADY_PIN} \
+    clockspeed=${SPI_CLOCKS_SPEED}
